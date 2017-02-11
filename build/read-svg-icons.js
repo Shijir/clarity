576a152ec4ac8197636f7f94feb5790e232ece0a
Shijir Tsogoo <stsogoo@vmware.com>
2017-02-10 16:55:28 -0800

    read file content is implemented

--- build/read-svg-icons.js
+++ build/read-svg-icons.js
@@ -14,41 +14,80 @@ const targetFileType = "svg";
 const pathToIcons = process.argv[2];
 
 /*
- * @desk synchronously checks whether directory exists or not
- * @param {string} path of directory
- * @return {boolean}
- * */
-let checkDirExist = (rawPath) => {
-
-    let absolutePath = path.resolve(rawPath);
-    
-    if (!fs.existsSync(absolutePath)) {
-        console.error(`'${absolutePath}' is not found.`);
-        return false;
-    }
-    if (!fs.statSync(path.resolve(rawPath)).isDirectory()) {
-        console.error(`'${absolutePath}' is not a directory.`);
-        return false;
-    }
-
-    return true;
-};
-
-/*
  * @desk read directory content and return list of files names
- * @param {string} path of directory
- * @resolve {string[]} file names in the directory
+ * @param {string} rawPath: path of directory
+ * @resolve {string[]}: file names in the directory
  * @reject {err}
  * */
-let readDirContent = () => {
+let readDirContent = (rawPath) => {
+
+    let absolutePath = path.resolve(rawPath);
+
+    return new Promise((resolve, reject)=> {
+
+        fs.readdir(absolutePath, (err, files)=> {
+
+            if (err) reject(err);
+
+            resolve(files);
+
+        });
+
+    });
+
 };
 
 /*
  * @desk reads file names and return their unique prefixes
- * @param {string[]} all file names
+ * @param {string[]} fileNamesParam: all file names
  * @return {string[]} prefixes
  * */
-let getPrefixes = () => {
+let getIconNames = (fileNamesParam) => {
+
+    let lineSuffix = "_line.svg";
+    let lineAlertSuffix = "_line_alert.svg";
+    let lineBadgeSuffix = "_line_badge.svg";
+
+    let solidSuffix = "_solid.svg";
+    let solidAlertSuffix = "_solid_alert.svg";
+    let solidBadgeSuffix = "_solid_badge.svg";
+
+    let iconNamesWithDuplicates = fileNamesParam.map((fileName)=> {
+
+
+        if (!/[A-Z0-9]/.test(fileName) && fileName.endsWith(lineSuffix)) {
+            return fileName.replace(lineSuffix, "");
+
+        }
+        else if (!/[A-Z0-9]/.test(fileName) && fileName.endsWith(lineAlertSuffix)) {
+            return fileName.replace(lineAlertSuffix, "");
+
+        }
+        else if (!/[A-Z0-9]/.test(fileName) && fileName.endsWith(lineBadgeSuffix)) {
+            return fileName.replace(lineBadgeSuffix, "");
+
+        }
+        else if (!/[A-Z0-9]/.test(fileName) && fileName.endsWith(solidSuffix)) {
+            return fileName.replace(solidSuffix, "");
+
+        }
+        else if (!/[A-Z0-9]/.test(fileName) && fileName.endsWith(solidAlertSuffix)) {
+            return fileName.replace(solidAlertSuffix, "");
+
+        }
+        else if (!/[A-Z0-9]/.test(fileName) && fileName.endsWith(solidBadgeSuffix)) {
+            return fileName.replace(solidBadgeSuffix, "");
+
+        }
+        else {
+            console.error(`'${fileName}' doesn't qualify as a standard name for icon files.`);
+        }
+
+    });
+
+    let iconSet = new Set(iconNamesWithDuplicates);
+    return Array.from(iconSet.values());
+
 };
 
 /*
@@ -56,8 +95,35 @@ let getPrefixes = () => {
  * @param {string} path of file
  * @return {string} file content
  * */
-let readFileContent = () => {
+let readFileContent = (pathToIconsParam, fileNameParam) => {
+
+    return new Promise((resolve, reject)=> {
+
+        fs.readFile(path.join(pathToIconsParam, fileNameParam), "utf-8", (err, data) => {
+            if (err) reject(err);
+
+            resolve({"fileName": fileNameParam, "content": data});
+        });
+
+    });
+
 };
 
-console.log(checkDirExist(pathToIcons));
+readDirContent(pathToIcons)
+    .then((fileNames)=> {
+        return Promise.all(fileNames.map((fileName)=> {
+
+            return readFileContent(pathToIcons, fileName);
+
+        }));
+
+    })
+    .then((fileContents)=> {
+
+        console.log(fileContents);
+
+    })
+    .catch((error)=> {
+        console.log(error);
+    });
 
