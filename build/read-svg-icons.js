563d394a5aa32aa0e73d5d2798cf005d7b340173
Shijir Tsogoo <stsogoo@vmware.com>
2017-02-11 01:04:33 -0800

    readDirContent is implemented

--- build/read-svg-icons.js
+++ build/read-svg-icons.js
@@ -9,7 +9,19 @@ const fs = require('fs');
 const path = require('path');
 const cheerio = require('cheerio');
 
-const targetFileType = "svg";
+const targetFileType = ".svg";
+const separatorCaseChar = "_";
+
+let lineSuffix = separatorCaseChar + "line" + targetFileType;
+let lineAlertSuffix = separatorCaseChar + "line_alert" + targetFileType;
+let lineBadgeSuffix = separatorCaseChar + "line_badge" + targetFileType;
+
+let solidSuffix = separatorCaseChar + "solid" + targetFileType;
+let solidAlertSuffix = separatorCaseChar + "solid_alert" + targetFileType;
+let solidBadgeSuffix = separatorCaseChar + "solid_badge" + targetFileType;
+
+let alertPathValue = "M26.85,1.14,21.13,11A1.28,1.28,0,0,0,22.23,13H33.68A1.28,1.28,0,0,0,34.78,11L29.06,1.14A1.28,1.28,0,0,0,26.85,1.14Z";
+
 
 const pathToIcons = process.argv[2];
 
@@ -44,46 +56,27 @@ let readDirContent = (rawPath) => {
  * */
 let getIconName = (fileNameParam) => {
 
-    let lineSuffix = "_line.svg";
-    let lineAlertSuffix = "_line_alert.svg";
-    let lineBadgeSuffix = "_line_badge.svg";
-
-    let solidSuffix = "_solid.svg";
-    let solidAlertSuffix = "_solid_alert.svg";
-    let solidBadgeSuffix = "_solid_badge.svg";
-
-
     if (!/[A-Z0-9]/.test(fileNameParam) && fileNameParam.endsWith(lineSuffix)) {
         return fileNameParam.replace(lineSuffix, "");
-
     }
     else if (!/[A-Z0-9]/.test(fileNameParam) && fileNameParam.endsWith(lineAlertSuffix)) {
         return fileNameParam.replace(lineAlertSuffix, "");
-
     }
     else if (!/[A-Z0-9]/.test(fileNameParam) && fileNameParam.endsWith(lineBadgeSuffix)) {
         return fileNameParam.replace(lineBadgeSuffix, "");
-
     }
     else if (!/[A-Z0-9]/.test(fileNameParam) && fileNameParam.endsWith(solidSuffix)) {
         return fileNameParam.replace(solidSuffix, "");
-
     }
     else if (!/[A-Z0-9]/.test(fileNameParam) && fileNameParam.endsWith(solidAlertSuffix)) {
         return fileNameParam.replace(solidAlertSuffix, "");
-
     }
     else if (!/[A-Z0-9]/.test(fileNameParam) && fileNameParam.endsWith(solidBadgeSuffix)) {
         return fileNameParam.replace(solidBadgeSuffix, "");
-
     }
     else {
-
-        console.error(`'${fileNameParam}' doesn't qualify as a standard name for icon files.`);
         return "";
-
     }
-
 
 };
 
@@ -127,11 +120,100 @@ let makeSVG = (shapeTitle, shapeContent) => {
 
 };
 
-let prepareIconContent = (shapeContent) => {
+let prepareIconContent = (fileNameParam, fileContentParam) => {
 
-    $ = cheerio.load(shapeContent);
 
-    return $("svg").html();
+    let elements = fileContentParam.match(/(<((title|path|circle|rect|line|polygon|polyline|ellipse)[^>]+\/>))/ig);
+
+
+    if (!/[A-Z0-9]/.test(fileNameParam) && fileNameParam.endsWith(lineSuffix)) {
+        return elements.map((element, index)=> {
+            return element
+                .replace(/(\/>)/ig, ` class="clr-i-outline clr-i-outline-path-${index + 1}" />`)
+                .replace(/fill="\#[a-z0-9]+"/ig, "");
+        });
+    }
+    else if (!/[A-Z0-9]/.test(fileNameParam) && fileNameParam.endsWith(lineAlertSuffix)) {
+
+        return elements.map((element, index)=> {
+
+            if (element.includes(alertPathValue)) {
+
+                return element
+                    .replace(/(\/>)/ig, ` class="clr-i-outline--alerted clr-i-outline-path-${index + 1}--alerted clr-i-alert" />`)
+                    .replace(/fill="\#[a-z0-9]+"/ig, "");
+
+            } else {
+
+                return element
+                    .replace(/(\/>)/ig, ` class="clr-i-outline--alerted clr-i-outline-path-${index + 1}--alerted" />`)
+                    .replace(/fill="\#[a-z0-9]+"/ig, "");
+
+            }
+        });
+
+    }
+    else if (!/[A-Z0-9]/.test(fileNameParam) && fileNameParam.endsWith(lineBadgeSuffix)) {
+
+        return elements.map((element, index)=> {
+            if (element.includes("circle") && element.includes(`r="5"`)) {
+                return element
+                    .replace(/(\/>)/ig, ` class="clr-i-outline--badged clr-i-outline-path-${index + 1}--badged clr-i-badge" />`)
+                    .replace(/fill="\#[a-z0-9]+"/ig, "");
+            } else {
+                return element
+                    .replace(/(\/>)/ig, ` class="clr-i-outline--badged clr-i-outline-path-${index + 1}--badged" />`)
+                    .replace(/fill="\#[a-z0-9]+"/ig, "");
+            }
+        });
+
+    }
+    else if (!/[A-Z0-9]/.test(fileNameParam) && fileNameParam.endsWith(solidSuffix)) {
+
+        return elements.map((element, index)=> {
+            return element
+                .replace(/(\/>)/ig, ` class="clr-i-solid clr-i-solid-path-${index + 1}" />`)
+                .replace(/fill="\#[a-z0-9]+"/ig, "");
+        });
+
+    }
+    else if (!/[A-Z0-9]/.test(fileNameParam) && fileNameParam.endsWith(solidAlertSuffix)) {
+
+        return elements.map((element, index)=> {
+            if (element.includes(alertPathValue)) {
+
+                return element
+                    .replace(/(\/>)/ig, ` class="clr-i-solid--alerted clr-i-solid-path-${index + 1}--alerted clr-i-alert" />`)
+                    .replace(/fill="\#[a-z0-9]+"/ig, "");
+
+            } else {
+
+                return element
+                    .replace(/(\/>)/ig, ` class="clr-i-solid--alerted clr-i-solid-path-${index + 1}--alerted" />`)
+                    .replace(/fill="\#[a-z0-9]+"/ig, "");
+
+            }
+        });
+
+    }
+    else if (!/[A-Z0-9]/.test(fileNameParam) && fileNameParam.endsWith(solidBadgeSuffix)) {
+
+        return elements.map((element, index)=> {
+            if (element.includes("circle") && element.includes(`r="5"`)) {
+                return element
+                    .replace(/(\/>)/ig, ` class="clr-i-solid--badged clr-i-solid-path-${index + 1}--badged clr-i-badge" />`)
+                    .replace(/fill="\#[a-z0-9]+"/ig, "");
+            } else {
+                return element
+                    .replace(/(\/>)/ig, ` class="clr-i-solid--badged clr-i-solid-path-${index + 1}--badged" />`)
+                    .replace(/fill="\#[a-z0-9]+"/ig, "");
+            }
+        });
+
+    }
+    else {
+
+    }
 
 };
 
@@ -147,15 +229,37 @@ readDirContent(pathToIcons)
     })
     .then((iconFileObjs)=> {
 
-        let icons = {};
+        let iconsWithArrayContent = {};
 
         iconFileObjs.map((iconFileObj)=> {
+
             let iconName = getIconName(iconFileObj["fileName"]);
 
-            if (iconName !== "" && icons[iconName] !== undefined) {
-                //exist
+            if (iconName !== "" && iconsWithArrayContent[iconName] !== undefined) {
+                iconsWithArrayContent[iconName] = iconsWithArrayContent[iconName].concat(prepareIconContent(iconFileObj["fileName"], iconFileObj["content"]));
             } else {
-                icons[iconName] = prepareIconContent(iconFileObj["content"]);
+                iconsWithArrayContent[iconName] = prepareIconContent(iconFileObj["fileName"], iconFileObj["content"]);
+            }
+
+        });
+
+        let iconNames = Object.keys(iconsWithArrayContent);
+
+        let icons = {};
+
+        iconNames.map((iconName)=> {
+
+
+            if (iconName !== "") {
+
+                let iconContent = iconsWithArrayContent[iconName].reduce((accumulator, currentValue)=> {
+
+                    return `${accumulator}
+                            ${currentValue}`;
+
+                });
+
+                icons[iconName] = makeSVG(iconName, iconContent);
             }
 
         });
