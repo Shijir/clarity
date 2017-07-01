cf38be24120aae396103ae782212517f12393549
Shijir Tsogoo <stsogoo@vmware.com>
2017-07-01 18:15:33 +0900

    nav link propagation is solved

--- src/clarity-angular/layout/vertical-nav/vertical-nav-link.directive.ts
+++ src/clarity-angular/layout/vertical-nav/vertical-nav-link.directive.ts
@@ -3,7 +3,7 @@
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
-import { Directive, Optional} from "@angular/core";
+import { Directive, Optional } from "@angular/core";
 import { VerticalNavGroup } from "./vertical-nav-group";
 
 @Directive({
@@ -11,7 +11,8 @@ import { VerticalNavGroup } from "./vertical-nav-group";
     host: {
         "class": "vertical-nav-item vertical-nav-link",
         "[class.icon-when-collapsed]": "hasIcon",
-        "[class.direct]": "isDirectItem"
+        "[class.direct]": "isDirectItem",
+        "(click)": "clicked($event)"
     }
 })
 export class VerticalNavLink {
@@ -23,6 +24,15 @@ export class VerticalNavLink {
         if (!this._parent) {
             this.isDirectItem = true;
         }
+    }
+
+    clicked($event) {
+
+        // Clicking on links, which are the children of the <nav>,
+        // should not propagate to the <nav> and causing it to expand.
+        // To expand <nav>, it should listen to the click events on itself, not one its children.
+
+        $event.stopPropagation();
     }
 
 }
