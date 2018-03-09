bb578827f5fde6ae50cb41338daa0e5ee496607b
Shijir Tsogoo <stsogoo@vmware.com>
2018-03-08 22:55:11 -0800

    animationbuilder is used
    Signed-off-by: Shijir Tsogoo <stsogoo@vmware.com>

--- src/clr-angular/utils/drag-and-drop/draggable-ghost.ts
+++ src/clr-angular/utils/drag-and-drop/draggable-ghost.ts
@@ -3,44 +3,63 @@
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
-import {animate, style, transition, trigger} from "@angular/animations";
-import {Component, ElementRef, HostBinding, Inject} from "@angular/core";
+
+import {animate, AnimationBuilder, AnimationPlayer, style, transition, trigger} from "@angular/animations";
+import {AfterViewInit, Component, ElementRef, HostBinding, Inject, OnDestroy} from "@angular/core";
+import {Subscription} from "rxjs/Subscription";
 
 import {DomAdapter} from "../../data/datagrid/render/dom-adapter";
 
+import {DraggableEvent} from "./interfaces/draggable-event";
 import {DragDispatcher} from "./providers/drag-dispatcher";
 import {CUSTOM_GHOST_STATE, CustomGhostState} from "./providers/draggable-tracker";
+
+const LEAVE_ANIMATION_SPEED = 200;  // 0.2ms
 
 @Component({
     selector: "clr-draggable-ghost",
     template: `
         <ng-content></ng-content>`,
-    host: {"[class.draggable-ghost]": "true", "[class.draggable-ghost--hidden]": "true", "[attr.focusable]": "false"},
-    animations:
-        [trigger("dragEndReset",
-                 [transition(":leave", [animate(200, style({top: "{{top}}", left: "{{left}}", opacity: 0}))])])]
+    host: {
+        "[class.draggable-ghost]": "true",
+        "[class.draggable-ghost--hidden]": "true",
+        "[attr.focusable]": "false",
+        "[@leaveAnimDuration]": "true"
+    },
+    animations: [trigger("leaveAnimDuration", [transition(":leave", [animate(LEAVE_ANIMATION_SPEED, null)])])]
 })
-export class DraggableGhost {
-    @HostBinding("@dragEndReset") dragEndResetHostAnim;
+export class DraggableGhost implements AfterViewInit, OnDestroy {
+    private subscriptions: Subscription[] = [];
 
     draggableGhostEl: Node;
 
+    leaveAnimation: AnimationPlayer;
+
     constructor(private el: ElementRef, private dragDispatcher: DragDispatcher, private domAdapter: DomAdapter,
-                @Inject(CUSTOM_GHOST_STATE) private customGhostState: CustomGhostState) {
+                @Inject(CUSTOM_GHOST_STATE) private customGhostState: CustomGhostState,
+                private builder: AnimationBuilder) {
         this.draggableGhostEl = el.nativeElement;
-
-        let originalLeft: string;
-        let originalTop: string;
-
-        if (customGhostState.isDirectChild) {
-            originalLeft = this.dragDispatcher.dragStartPosition.x + "px";
-            originalTop = this.dragDispatcher.dragStartPosition.y + "px";
-        } else {
-            originalLeft = this.domAdapter.clientRectLeft(this.dragDispatcher.draggable.self) + "px";
-            originalTop = this.domAdapter.clientRectTop(this.dragDispatcher.draggable.self) + "px";
-        }
+        console.log(this.draggableGhostEl);
 
 
-        this.dragEndResetHostAnim = {value: ":leave", params: {left: originalLeft, top: originalTop}};
+        // this.subscriptions.push(dragDispatcher.onDragEnd.subscribe(($event: DraggableEvent) => {
+        //     this.container.clear();
+        // }));
+    }
+
+    ngAfterViewInit() {
+        const originalLeft = this.domAdapter.clientRectLeft(this.dragDispatcher.draggable.self) + "px";
+        const originalTop = this.domAdapter.clientRectTop(this.dragDispatcher.draggable.self) + "px";
+
+        const leaveAnimationFactory = this.builder.build([
+            style({left: "*", top: "*"}),
+            animate(LEAVE_ANIMATION_SPEED, style({left: originalLeft, top: originalTop}))
+        ]);
+
+        this.leaveAnimation = leaveAnimationFactory.create(this.draggableGhostEl);
+    }
+
+    ngOnDestroy() {
+        this.leaveAnimation.play();
     }
 }
