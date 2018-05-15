5170fbc9584531c87d3fd9ad1a71e99869745c4c
Shijir Tsogoo <stsogoo@vmware.com>
2018-05-14 22:07:13 -0700

    added in test files
    Signed-off-by: Shijir Tsogoo <stsogoo@vmware.com>

--- src/clr-angular/utils/drag-and-drop/all.spec.ts
+++ src/clr-angular/utils/drag-and-drop/all.spec.ts
@@ -3,6 +3,7 @@
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
+import ClrDragHandleSpecs from "./drag-handle.spec";
 import ClrIfDraggedSpecs from "./if-dragged.spec";
 import ClrDragAndDropEventBusSpecs from "./providers/drag-and-drop-event-bus.spec";
 import ClrDragEventListenerSpecs from "./providers/drag-event-listener.spec";
@@ -16,5 +17,6 @@ fdescribe("Drag And Drop", function() {
     });
     describe("Components And Directives", function() {
         ClrIfDraggedSpecs();
+        ClrDragHandleSpecs();
     });
 });
