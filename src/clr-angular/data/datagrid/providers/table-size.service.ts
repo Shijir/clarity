9ebd9963a78ec102036cbc5195eb70c009707f9f
Shijir Tsogoo <stsogoo@vmware.com>
2018-08-14 13:25:00 -0700

    table-size.service
    Signed-off-by: Shijir Tsogoo <stsogoo@vmware.com>

---
+++ src/clr-angular/data/datagrid/providers/table-size.service.ts
@@ -0,0 +1,29 @@
+
+/*
+ * Copyright (c) 2016-2018 VMware, Inc. All Rights Reserved.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+import {isPlatformBrowser} from "@angular/common";
+import {ElementRef, Inject, Injectable, PLATFORM_ID} from "@angular/core";
+
+/**
+ * @description
+ * Internal datagrid service that holds a reference to the clr-dg-table element and exposes a method to get height.
+ */
+@Injectable()
+export class TableSizeService {
+    private tableRef: Element;
+
+    constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+    public set table(table: ElementRef) {
+        if (isPlatformBrowser(this.platformId) && table.nativeElement) {
+            this.tableRef = table.nativeElement.querySelector(".datagrid-table");
+        }
+    }
+
+    // Used when resizing columns to show the column border being dragged.
+    getColumnDragHeight(): string {
+        return `${this.tableRef.clientHeight}px`;
+    }
+}
