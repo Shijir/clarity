ec0d297057732f923d72d909a61f2d4ff9f842f9
Jeremy Wilken <gnomation@gnomeontherun.com>
2020-01-27 16:21:06 -0600

    chore: ngcc cleanup and dark theme dev app fixes
    Signed-off-by: Jeremy Wilken <gnomation@gnomeontherun.com>

--- scripts/ngcc-clean.js
+++ scripts/ngcc-clean.js
@@ -8,21 +8,15 @@
 
 const fs = require('fs');
 
+const files = require('./ngcc');
+
 // We have to run ngcc during postinstall since it does not support parallel builds https://github.com/angular/angular/issues/32431
 // ngcc pollutes package.json files in the monorepo use case https://github.com/angular/angular/issues/33395
 // this script removes the breaking generated properties from ngcc
 
-cleanNGCCPollution('./src/clr-angular/package.json');
-cleanNGCCPollution('./src/clr-ui/package.json');
-cleanNGCCPollution('./src/clr-icons/package.json');
-cleanNGCCPollution('./src/clr-core/package.json');
-cleanNGCCPollution('./src/clr-core/badge/package.json');
-cleanNGCCPollution('./src/clr-core/button/package.json');
-cleanNGCCPollution('./src/clr-core/common/package.json');
-cleanNGCCPollution('./src/clr-core/icon-shapes/package.json');
-cleanNGCCPollution('./src/clr-core/icon/package.json');
-cleanNGCCPollution('./src/clr-core/tag/package.json');
-cleanNGCCPollution('./src/clr-core/test-dropdown/package.json');
+files.forEach(pkg => {
+  cleanNGCCPollution(pkg);
+});
 
 function cleanNGCCPollution(file) {
   const data = JSON.parse(fs.readFileSync(file));
