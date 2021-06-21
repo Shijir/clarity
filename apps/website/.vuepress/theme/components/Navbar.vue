0836234d87c42ce7bff6ff67ca845851c7065339
stsogoo <stsogoo@vmware.com>
2021-06-21 10:14:22 -0700

    fix: update ng site url
    Signed-off-by: stsogoo <stsogoo@vmware.com>

--- apps/website/.vuepress/theme/components/Navbar.vue
+++ apps/website/.vuepress/theme/components/Navbar.vue
@@ -52,7 +52,7 @@
 import AlgoliaSearchBox from '@AlgoliaSearchBox';
 import SearchBox from '@SearchBox';
 
-const defaultAngularSiteURL = 'https://angular.clarity.design/';
+const defaultAngularSiteURL = 'https://60d0c580ebc545b6c2b6e8ec--angular-clarity-design.netlify.app/';
 
 export default {
   name: 'Navbar',
